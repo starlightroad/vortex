08b3f203f6ee5c5480a57ca416a0185aa95d04a2
blackOw1 <ortizge92@gmail.com>
2023-09-11 12:46:00 -0400

    Add email validation

---
+++ src/lib/email.ts
@@ -0,0 +1,34 @@
+import validate from "deep-email-validator";
+import { createError } from "./utils";
+
+const validateEmail = async (email: string, sender?: string) => {
+  try {
+    const res = await validate({
+      email,
+      sender,
+      validateRegex: true,
+      validateMx: true,
+      validateTypo: true,
+      validateDisposable: true,
+      validateSMTP: true,
+    });
+
+    const data = {
+      isEmailValid: res.valid,
+      validators: {
+        isRegexValid: res.validators.regex?.valid,
+        isTypoValid: res.validators.typo?.valid,
+        isDisposableValid: res.validators.disposable?.valid,
+        isMxValid: res.validators.mx?.valid,
+      },
+    };
+
+    return data;
+  } catch (err) {
+    const error = createError(err);
+
+    throw new Error(error.message);
+  }
+};
+
+export default validateEmail;
