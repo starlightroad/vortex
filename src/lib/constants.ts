ee6c329a2c0577af548b4eed655c0dc5c52b587c
blackOw1 <ortizge92@gmail.com>
2023-09-16 11:45:12 -0400

    Update sign in failed constant and add email invalid  constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,11 +2,12 @@ export const emailConstants = {
   EMAIL_EXISTS: "Email is taken",
   EMAIL_FAILED_TO_SEND: "Email could not be sent",
   EMAIL_NOT_FOUND: "Couldn't find your account",
+  EMAIL_INVALID: "Invalid email",
 };
 
 export const signInConstants = {
   SIGN_IN_SUCCESS: "Please check your email to sign in",
-  SIGN_IN_FAILED: "Invalid email",
+  SIGN_IN_FAILED: emailConstants.EMAIL_INVALID,
 };
 
 export const dbConstants = {
